625ad4fd309be52ddd62399f6fe9cb332dc39c1e
danielazbib <danielazbib@gmail.com>
2020-06-03 09:37:49 -0400

    Add files via upload

---
+++ classCodes.java
@@ -0,0 +1,12 @@
+class classCodes {
+    
+    //core class codes for english, social studies, math, and science, 9-11
+    String[] eCodes = {"EES81","EES82","EES83","EES84","EES85","EES86","EES85X","EES86X"};
+    String[] ssCodes = {"HGS41","HSG42","HSG43","HGS44","HUS21","HUS22"};
+    String[] mCodes = {"MES21","MES22","MGS21","MGS22","MRS21","MRS22"};
+    String[] sCodes = {"SLS21","SLS22","SCS21","SCS22","SPS21","SPS22"};
+
+    //2d array of all class codes
+    String[][] clsCodes = {eCodes, ssCodes, mCodes, sCodes};
+  
+}
